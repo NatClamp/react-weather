bb281e178b7dd314241fba330942a4ff636597c7
NatClamp <nat.clamp@gmail.com>
2018-12-18 13:44:16 +0000

    swapping over

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import CityOptions from './components/CityOptions';
 import Chart from './components/Chart';
-import data from './data/cityCodes.json'
+import data from './data/cityCodes.json';
+import { WEATHER_KEY } from './config.js'
 
 class App extends Component {
   state = {
+    areaWeather: [],
     currentCityID : '',
   }
 
@@ -18,6 +21,16 @@ class App extends Component {
       </div>
     );
   }
+
+
+
+fetchWeather = () => {
+if (this.state.currentCityID !== '') return axios.get(`api.openweathermap.org/data/2.5/forecast?id=${this.state.currentCityID}&APPID=${WEATHER_KEY}`).then(console.log)
+}
+
+componentDidMount() {
+  this.fetchWeather();
+}
 
 chooseArea = (chosenAreaName) => {
   const area = data.filter(area => {
